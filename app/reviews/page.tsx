f5fbcb8a7cfd15444dfb09214afdb155de46a613
Milad Moulayi <m.moulayi123@gmail.com>
2023-08-31 14:57:03 -0700

    add links

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
+
 export default function ReviewsPage() {
   return (
     <>
       <h1>Reviews</h1>
-      <p>Reviews go here</p>
+      <ul>
+        <li>
+          <Link href="/reviews/stardew-valley">Stardew Valley</Link>
+        </li>
+        <li>
+          <Link href="/reviews/hollow-knight">Hollow Knight</Link>
+        </li>
+      </ul>
     </>
   );
 }
